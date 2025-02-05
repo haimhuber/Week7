eb2b8b8a6ad35fb67f7cde71143704aacceea8fe
= <=>
2025-02-05 09:48:00 +0200

    commit

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ function function1() {
     newElement.textContent = 'Hello world';
     document.querySelector("body").appendChild(newElement); // מוסיף את האלמנט לסוף האלמנט
 }
+
+function function2() {
+
+    const referenceElement = document.querySelector('#reference');
+    const newElement = document.createElement('p');
+    newElement.textContent = 'I am a sibling!';
+
+    // הוספה אחרי האלמנט
+    referenceElement.parentNode.insertBefore(newElement, referenceElement.nextSibling);
+
+}
