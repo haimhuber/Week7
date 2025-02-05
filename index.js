aa3c89285e7e6b0d8d20a008af535e7386252c0f
= <=>
2025-02-05 14:33:04 +0200

    done

--- index.js
+++ index.js
@@ -1,36 +1,67 @@
 function function1() {
-    const parentElement = document.querySelector("#parent");
-    const newElement = document.createElement('h2');
-    newElement.textContent = "I am h2"
-    parentElement.parentNode.appendChild(newElement);
+    const newElement = document.createElement("h1");
+    newElement.textContent = "Hello";
+    document.querySelector("#container").parentNode.appendChild(newElement);
 }
-function1();
 
 function function2() {
-    const newDiv = document.createElement('div');
-    newDiv.textContent = "I am Div 1";
-    document.querySelector("#parent").insertBefore(newDiv, document.querySelector("#sibling"));
-}
-function2();
-
-
-function function3() {
-    const newDiv = document.createElement('div');
-    newDiv.textContent = "I am Div 2";
-    document.querySelector("#parent").insertBefore(newDiv, document.querySelector("#sibling").nextSibling);
-}
-function3();
-
-function functio4() {
-    const child = document.querySelector('#child');
-    child.style.color = 'blue';
-}
-functio4();
-
-function functio5() {
-    const child = document.querySelectorAll('[id*="sibling"]');
-    child.forEach(element => {
-        element.style.fontSize = '18px';
+    const changeColor = document.querySelectorAll('[id*="p"]')
+    changeColor.forEach(curr => {
+        curr.style.color = 'red';
     });
 }
-functio5();
+
+function function3() {
+    document.querySelector("#p3").remove();
+}
+
+function function4() {
+    const changeColor = document.querySelectorAll('[id*="p"]')
+    changeColor.forEach(curr => {
+        curr.style.border = '2px black solid';
+    });
+}
+
+function function5() {
+    const button = document.querySelector("#clickMe");
+    button.textContent = "Clicked";
+}
+
+function function6() {
+    const theBody = document.querySelector("body");
+    theBody.addEventListener('click', () => {
+        document.querySelector("body").style.backgroundColor = 'yellow';
+    });
+
+}
+
+function function7() {
+    const newul = document.createElement('ul');
+    const newli = ['item1', 'item2', 'item3'];
+    newli.forEach(li => {
+        const newil = document.createElement('li');
+        newil.textContent = li;
+        newul.appendChild(newil);
+    });
+    document.querySelector('#container').parentNode.appendChild(newul);
+}
+
+function function8() {
+    const newPicsrc = document.createElement('img');
+    newPicsrc.setAttribute("src", "https://images.pexels.com/photos/30462129/pexels-photo-30462129/free-photo-of-majestic-himalayan-mountain-landscape.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load");
+    newPicsrc.style.width = '300px';
+    newPicsrc.style.height = '300px';
+    document.querySelector("#container").parentNode.appendChild(newPicsrc);
+}
+
+
+function function9() {
+    const fontChange = document.querySelectorAll('[id*="p"]');
+    fontChange.forEach(curr => {
+        curr.style.fontFamily = "Arial, Helvetica, sans-serif";
+    });
+}
+
+function function10() {
+    document.querySelector("#clickMe").remove();
+}
